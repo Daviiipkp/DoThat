6d12864a282fd74415981f78c8d640aa5c7cddab
Daviiipkp <daviipkp@outlook.com>
2026-09-16 02:39:37 -0300

    removed one return;

--- src/main/java/org/daviipkp/dothat/ActionManager.java
+++ src/main/java/org/daviipkp/dothat/ActionManager.java
@@ -43,7 +43,6 @@ public class ActionManager {
             try{
                 fillAction(act, q.getArgs());
             }catch(Exception e) {
-                return; //TODO
             }
         }
         act.execute();
